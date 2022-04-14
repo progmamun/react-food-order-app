d37c5b3ac443245e6eef87f2785b596c245f7409
progmamun <mamun333089@gmail.com>
2022-04-15 05:30:46 +0600

    meal-components

---
+++ src/components/Meals/Meals.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MealsSummary from './MealsSummary';
+import AvailableMeals from './AvailableMeals';
+
+const Meals = () => {
+  return (
+    <>
+      <MealsSummary></MealsSummary>
+      <AvailableMeals></AvailableMeals>
+    </>
+  );
+};
+
+export default Meals;
