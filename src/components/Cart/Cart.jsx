9b35c366143b374615ba8d70559e19053e476cd8
progmamun <mamun333089@gmail.com>
2022-04-15 23:40:40 +0600

    remove-items

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -10,15 +10,21 @@ const Cart = props => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = id => {};
-  const cartItemAddHandler = item => {};
+  const cartItemRemoveHandler = id => {
+    cartCtx.removeItem(id);
+  };
+  const cartItemAddHandler = item => {
+    cartCtx.addItem(item);
+  };
 
   const cartItems = (
     <ul className={classes['cart-items']}>
       {cartCtx.items.map(item => (
         <CartItem
           key={item.id}
-          item={item}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
           onRemove={cartItemRemoveHandler.bind(null, item.id)}
           onAdd={cartItemAddHandler.bind(null, item)}
         ></CartItem>
